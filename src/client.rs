1133faf7a7d239544665ac1b14419082a3326f13
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-02-03 19:12:23 +0100

    Split structure and add fetch subcommand

---
+++ src/client.rs
@@ -0,0 +1,105 @@
+use anyhow::{anyhow, Result};
+use regex::Regex;
+use reqwest;
+use select::document::Document;
+use select::node::Node;
+use select::predicate::{Attr, Class, Name, Predicate};
+
+pub struct Street {
+    pub name: String,
+    pub date: String,
+}
+
+pub struct Query {
+    pub street: String,
+    pub value: String,
+}
+
+impl Query {
+    fn from(node: Node) -> Option<Query> {
+        if let Some(value) = node.attr("value") {
+            return Some(Query {
+                street: node.text().trim().to_string(),
+                value: value.to_string(),
+            });
+        }
+        None
+    }
+}
+
+pub struct Client {
+    client: reqwest::Client,
+    url: &'static str,
+    date_expr: Regex,
+}
+
+impl Client {
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            client: reqwest::Client::new(),
+            url: "https://web6.karlsruhe.de/service/abfall/akal/akal.php",
+            date_expr: Regex::new(r"(\d\d\.\d\d\.2021)")?,
+        })
+    }
+
+    pub async fn queries(&self) -> Result<Vec<Query>> {
+        let text = self
+            .client
+            .post(self.url)
+            .query(&[("von", "A"), ("bis", "[")])
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let document = Document::from(text.as_str());
+
+        Ok(document
+            .find(Class("tab_body").descendant(Name("option")))
+            .into_iter()
+            .filter_map(|node| Query::from(node))
+            .collect::<Vec<_>>())
+    }
+
+    pub async fn get_date(&self, query: Query) -> Result<Street> {
+        let data = [
+            ("anzeigen", "anzeigen"),
+            ("strasse", &query.value),
+            ("hausnr", ""),
+        ];
+
+        let text = self
+            .client
+            .post(self.url)
+            .query(&[("von", "A"), ("bis", "[")])
+            .form(&data)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let node = Document::from(text.as_str())
+            .find(Attr("id", "foo").descendant(Name("tbody")))
+            .next()
+            .ok_or(anyhow!("<div id='foo'><tbody> not found"))?
+            .last_child() // last <tr>
+            .ok_or(anyhow!("<tr> not found"))?
+            .find(Name("td"))
+            .skip(2)
+            .next()
+            .ok_or(anyhow!("third <td> not found"))?
+            .text();
+
+        Ok(Street {
+            date: self
+                .date_expr
+                .captures(&node)
+                .ok_or(anyhow!("No date found for {}", query.street))?
+                .get(0)
+                .ok_or(anyhow!("foo"))?
+                .as_str()
+                .to_owned(),
+            name: query.street,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,40 @@
-use anyhow::{anyhow, Result};
-use regex::Regex;
-use reqwest;
-use select::document::Document;
-use select::node::Node;
-use select::predicate::{Attr, Class, Name, Predicate};
+mod client;
+
+use anyhow::Result;
+use client::Client;
+use structopt::StructOpt;
 use tokio;
 
-struct StreetQuery {
-    street: String,
-    value: String,
+#[derive(StructOpt)]
+enum Commands {
+    #[structopt(about = "Fetch dates")]
+    Fetch,
 }
 
-impl StreetQuery {
-    fn from(node: Node) -> Option<StreetQuery> {
-        if let Some(value) = node.attr("value") {
-            return Some(StreetQuery {
-                street: node.text().trim().to_string(),
-                value: value.to_string(),
-            });
-        }
-        None
-    }
-}
-
-struct Client {
-    client: reqwest::Client,
-    url: &'static str,
-    date_expr: Regex,
-}
-
-impl Client {
-    fn new() -> Result<Self> {
-        Ok(Self {
-            client: reqwest::Client::new(),
-            url: "https://web6.karlsruhe.de/service/abfall/akal/akal.php",
-            date_expr: Regex::new(r"(\d\d\.\d\d\.2021)")?,
-        })
-    }
-
-    async fn queries(&self) -> Result<Vec<StreetQuery>> {
-        let text = self
-            .client
-            .post(self.url)
-            .query(&[("von", "A"), ("bis", "[")])
-            .send()
-            .await?
-            .text()
-            .await?;
-
-        let document = Document::from(text.as_str());
-
-        Ok(document
-            .find(Class("tab_body").descendant(Name("option")))
-            .into_iter()
-            .filter_map(|node| StreetQuery::from(node))
-            .collect::<Vec<_>>())
-    }
-
-    async fn get_date(&self, query: &StreetQuery) -> Result<String> {
-        let data = [
-            ("anzeigen", "anzeigen"),
-            ("strasse", &query.value),
-            ("hausnr", ""),
-        ];
-
-        let text = self
-            .client
-            .post(self.url)
-            .query(&[("von", "A"), ("bis", "[")])
-            .form(&data)
-            .send()
-            .await?
-            .text()
-            .await?;
-
-        let node = Document::from(text.as_str())
-            .find(Attr("id", "foo").descendant(Name("tbody")))
-            .next()
-            .ok_or(anyhow!("<div id='foo'><tbody> not found"))?
-            .last_child() // last <tr>
-            .ok_or(anyhow!("<tr> not found"))?
-            .find(Name("td"))
-            .skip(2)
-            .next()
-            .ok_or(anyhow!("third <td> not found"))?
-            .text();
-
-        Ok(self
-            .date_expr
-            .captures(&node)
-            .ok_or(anyhow!("No date found for {}", query.street))?
-            .get(0)
-            .ok_or(anyhow!("foo"))?
-            .as_str()
-            .to_owned())
-    }
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
+async fn fetch() -> Result<()> {
     let client = Client::new()?;
 
     for query in client.queries().await? {
-        match client.get_date(&query).await {
-            Ok(date) => {
-                println!("{};{}", query.street, date);
+        match client.get_date(query).await {
+            Ok(street) => {
+                println!("{};{}", street.name, street.date);
             }
-            Err(_) => {
-                println!("{};n/a", query.street);
-            }
+            Err(_) => {}
         }
     }
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let commands = Commands::from_args();
+
+    let result = match commands {
+        Commands::Fetch {} => fetch().await,
+    };
+
+    if let Err(err) = result {
+        eprintln!("\x1B[2K\r\x1B[0;31mError\x1B[0;m {}", err);
+    }
 }
