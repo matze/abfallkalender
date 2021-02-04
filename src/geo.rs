ab39b52bc84a4998efd053b6d3e54ef51de4c8ee
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-02-04 22:35:16 +0100

    Only include streets in a 10km radius

--- src/geo.rs
+++ src/geo.rs
@@ -11,6 +11,11 @@ pub struct Point {
     pub lon: f64,
 }
 
+const ORIGIN: Point = Point {
+    lat: 49.0f64,
+    lon: 8.40f64,
+};
+
 #[derive(Serialize, Deserialize)]
 struct StreetIds {
     street: Street,
@@ -22,6 +27,19 @@ pub struct StreetPoints {
     pub name: String,
     pub date: String,
     pub segments: Vec<Vec<Point>>,
+}
+
+fn distance(a: &Point, b: &Point) -> f64 {
+    let mut ma = a.clone();
+    let mut mb = b.clone();
+    ma.lon -= mb.lon;
+    ma.lon = ma.lon.to_radians();
+    ma.lat = ma.lat.to_radians();
+    mb.lat = mb.lat.to_radians();
+    let dz: f64 = ma.lat.sin() - mb.lat.sin();
+    let dx: f64 = ma.lon.cos() * ma.lat.cos() - mb.lat.cos();
+    let dy: f64 = ma.lon.sin() * ma.lat.cos();
+    ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * 6372.8
 }
 
 fn get_value(tags: &mut TagIter, key: &str) -> Option<String> {
@@ -40,9 +58,18 @@ fn convert_segment_ids(ids: Vec<i64>, map: &HashMap<i64, Point>) -> Vec<Point> {
         .collect::<Vec<Point>>()
 }
 
+fn valid_segment(segment: &Vec<i64>, map: &HashMap<i64, Point>) -> bool {
+    if let Some(first) = segment.get(0) {
+        return distance(map.get(first).unwrap(), &ORIGIN) < 10.0f64;
+    }
+
+    false
+}
+
 fn convert_segments(segments: Vec<Vec<i64>>, map: &HashMap<i64, Point>) -> Vec<Vec<Point>> {
     segments
         .into_iter()
+        .filter(|segment| valid_segment(&segment, map))
         .map(|segment| convert_segment_ids(segment, map))
         .collect::<Vec<Vec<Point>>>()
 }
